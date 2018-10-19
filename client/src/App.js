380c2c8c95c63e00d755277822752e5352789c8e
Babita Bisht <babitabisht2018@gmail.com>
2018-10-19 17:18:34 +0530

    Client Added

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AppNavBar from "./components/AppNavBar";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AppNavBar />
+      </div>
+    );
+  }
+}
+
+export default App;
